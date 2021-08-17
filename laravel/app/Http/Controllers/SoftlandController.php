b75f85f9556b940104f4509c56de733e9f614005
1 <1@m.com>
2021-08-17 16:59:28 -0400

    editado codigo

--- laravel/app/Http/Controllers/SoftlandController.php
+++ laravel/app/Http/Controllers/SoftlandController.php
@@ -9,66 +9,46 @@ use App\Sync;
 
 class SoftlandController extends Controller
 {
-    //
-    const IVA = 1.19;
-    private $accessToken;
-    private $endPoint;
-    public function __construct(
-    )
+
+    public function index()
     {
-      $this->endPoint = env('LAUDUS_ENDPOINT') != '' ? env('LAUDUS_ENDPOINT') : 'https://erp.laudus.cl/api/';
-      $this->user = env('LAUDUS_USER') != '' ? env('LAUDUS_USER') : '';
-      $this->password = env('LAUDUS_PASSWORD') != '' ? env('LAUDUS_PASSWORD') : '';
-      $this->companyVatId = env('LAUDUS_COMPANY_VAT_ID') != '' ? env('LAUDUS_COMPANY_VAT_ID') : '';
-      $this->wharehouseId = env('LAUDUS_WAREHOUSE_ID') != '' ? env('LAUDUS_WAREHOUSE_ID') : '';
-      $this->laudusToken = null;
-    }
-    public function index(){
-        return $this->getObtenerProductosPorDescripcion();
-    }
-    public function config(){
-        /* echo 'user = '.env('LAUDUS_USER').'<br>';
-        echo 'password = '.env('LAUDUS_PASSWORD').'<br>'; */
-        echo 'companyVatId = '.env('LAUDUS_COMPANY_VAT_ID').'<br>';
-        echo 'wharehouseId = '.env('LAUDUS_WAREHOUSE_ID').'<br>';
+        return $this->getObtenerCatalogoProductosResponse();
     }
 
-    private function getObtenerProductosPorDescripcion() {
-        $dataRaw = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sof="http://softland.cl/">
-        <soapenv:Header>
-           <sof:AuthHeader>
-              <!--Optional:-->
-              <sof:Username>STORE</sof:Username>
-              <!--Optional:-->
-              <sof:Password>softland</sof:Password>
-           </sof:AuthHeader>
-        </soapenv:Header>
-        <soapenv:Body>
-           <sof:ObtenerProductosPorDescripcion>
-              <!--Optional:-->
-              <sof:codigo>para</sof:codigo>
-              <!--Optional:-->
-              <sof:Empresa>CORSE1</sof:Empresa>
-              <!--Optional:-->
-              <sof:Token></sof:Token>
-           </sof:ObtenerProductosPorDescripcion>
-        </soapenv:Body>
-     </soapenv:Envelope>';
+    private function getObtenerCatalogoProductosResponse()
+    {
+        $dataRaw = '<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Header>
+    <AuthHeader xmlns="http://softland.cl/">
+      <Username>STORE</Username>
+      <Password>softland</Password>
+    </AuthHeader>
+  </soap:Header>
+  <soap:Body>
+    <ObtenerCatalogoProductos xmlns="http://softland.cl/">
+      <Empresa>string</Empresa>
+      <esParaWeb>False</esParaWeb>
+      <CodBode>string</CodBode>
+      <listaPrecio>string</listaPrecio>
+      <soloActivos>False</soloActivos>
+    </ObtenerCatalogoProductos>
+  </soap:Body>
+</soap:Envelope>';
         $url = 'http://web.softlandcloud.cl/ecommerce/WSProducto.asmx?WSDL';
         $response = postSoapCurlRequest($url, null, $dataRaw);
         $response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $response);
         $xml = new \SimpleXMLElement($response);
-        if ($xml->soapBody && $xml->soapBody->ObtenerProductosPorDescripcionResponse && $xml->soapBody->ObtenerProductosPorDescripcionResponse->ObtenerProductosPorDescripcionResult && $xml->soapBody->ObtenerProductosPorDescripcionResponse->ObtenerProductosPorDescripcionResult->productoUni) {
-            $results = $xml->soapBody->ObtenerProductosPorDescripcionResponse->ObtenerProductosPorDescripcionResult;
+        if ($xml->soapBody && $xml->soapBody->ObtenerCatalogoProductosResponse && $xml->soapBody->ObtenerCatalogoProductosResponse->ObtenerCatalogoProductosResult && $xml->soapBody->ObtenerCatalogoProductosResponse->ObtenerCatalogoProductosResult->productoUni) {
+            $results = $xml->soapBody->ObtenerCatalogoProductosResponse->ObtenerCatalogoProductosResult;
             $products = [];
-            foreach ($results->productoUni as $key=>$val) {
+            foreach ($results->productoUni as $key => $val) {
                 $products[] = $val;
             }
             // return json_encode($products);
             return response()->json($products, 200);
         } else {
             dd($xml);
-        }        
+        }
     }
-    
 }
